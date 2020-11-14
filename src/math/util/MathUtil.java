10ad7a83212671b30b788556a52938711a011d1f
dobinhphuoc021000 <michealckem1@gmail.com>
2020-11-14 12:12:43 +0700

    2nd: import static modification

--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -6,7 +6,9 @@
 package math.util;
 
 import java.util.Scanner;
-import phuoc.util.MathUtility;
+import static phuoc.util.MathUtility.*;
+// câu lệnh từ JDK 8 (5), khai báo sẵn hàm static
+// ở dưới gọi hàm static ko cần chấm, y chang C, chỉ áp dụng cho static
 
 /**
  *
@@ -20,14 +22,15 @@ public class MathUtil {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long expected = 120; // mong nhận về 120 nếu gọi hàm 5!
-        long actual = MathUtility.getFactorial(5);
+        long actual = getFactorial(5);
         System.out.println("5!: Expected: " + expected + "; Actual: " + actual);
-        
-        System.out.println("6!: " + MathUtility.getFactorial(6));
-        System.out.println("0!: " + MathUtility.getFactorial(0));
-        
+
+        System.out.println("6!: " + getFactorial(6));
+        System.out.println("0!: " + getFactorial(0));
+
         // expected: IllegalArgurmentException if -5!
-        System.out.println("-5!: " + MathUtility.getFactorial(-5));
+        System.out.println("-5!: " + getFactorial(-5));
+        System.out.println("Pi: " + PI);
     }
 
 }
